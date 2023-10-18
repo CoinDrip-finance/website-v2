5de5fef61bd22599a427b40a7ba9e8e78a2891c9
Razvan Statescu <razvan@statescu.net>
2023-10-18 23:07:49 +0300

    add v1 link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { APP_URL } from "@/config";
+import { APP_URL, V1_APP_URL } from "@/config";
 
 export default function Navbar() {
   return (
@@ -7,9 +7,6 @@ export default function Navbar() {
       <div className="section flex items-center justify-between py-12">
         <img src="/logo.svg" alt="Coindrip" className="h-8 sm:h-auto" />
         <div className="flex items-center space-x-5 sm:space-x-8">
-          {/* <a href={V1_APP_URL} target="_blank" className="text-sm font-semibold">
-            <span className="hidden sm:inline-block">Go to</span> V1
-          </a> */}
           <div className="items-center space-x-3 hidden sm:flex">
             <a href="https://twitter.com/CoinDripHQ" target="_blank">
               <img src="/navbar/twitter.svg" alt="Twitter" className="h-6" />
@@ -18,6 +15,13 @@ export default function Navbar() {
               <img src="/navbar/linkedin.svg" alt="Twitter" className="h-6" />
             </a>
           </div>
+          <a
+            href={V1_APP_URL}
+            target="_blank"
+            className="text-sm font-semibold py-2  px-4 sm:px-6 bg-neutral-800 rounded-lg hover:bg-opacity-75"
+          >
+            <span className="hidden sm:inline-block">Go to</span> V1
+          </a>
           <a
             href={APP_URL}
             target="_blank"

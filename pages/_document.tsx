64c161422d7f2beff3010849b50bc60bd3510e20
Razvan Statescu <razvan@statescu.net>
2023-10-23 10:45:06 +0300

    add analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="shortcut icon" href="/favicon.png" />
+
+        <script defer data-domain="coindrip.finance" src="https://plausible.io/js/script.js" />
       </Head>
       <body>
         <Main />
